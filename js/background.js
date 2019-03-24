b8ae732db4d52ee786058fd5e86c698e88fbbf1e
matico <mactiencong@gmail.com>
2019-03-24 23:24:35 +0900

    Update

--- js/background.js
+++ js/background.js
@@ -1,18 +1,9 @@
-browser.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        if(isBlockUrl(details)){
-            return {cancel: true}
-        }
-    },
-    {urls: ["<all_urls>"]},
-    ["blocking"]
-);
-const url = browser.runtime.getURL('../data/block_urls.json');
+const url = chrome.runtime.getURL('../data/block_urls.json');
 let block_urls = []
 fetch(url)
     .then((response) => response.json())
     .then((data) => {
-        ads_urls = data
+        block_urls = data
     });
 function isBlockUrl(details){
     for (let index = 0; index < block_urls.length; index++) {
@@ -22,3 +13,46 @@ function isBlockUrl(details){
     }
     return false
 }
+
+function block(details) {
+    if(isBlockUrl(details)){
+        return {cancel: true}
+    }
+}
+
+let isEnable = true
+function enable(){
+    isEnable = true
+    setEnableIcon()
+    chrome.webRequest.onBeforeRequest.addListener(
+        block,
+        {urls: ["<all_urls>"]},
+        ["blocking"]
+    );
+}
+
+function setEnableIcon(){
+    chrome.browserAction.setIcon({
+        path : "icon/enable.png"
+    });
+}
+
+enable()
+
+function disable(){
+    isEnable = false
+    setDisableIcon()
+    chrome.webRequest.onBeforeRequest.removeListener(block)
+}
+
+function setDisableIcon(){
+    chrome.browserAction.setIcon({
+        path : "icon/disable.png"
+    });
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    if(isEnable) {
+        disable()
+    } else enable()
+});
